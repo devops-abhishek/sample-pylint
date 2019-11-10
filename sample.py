e26aa202dbf6444cf8beb12240c4b3c292af587c
chordia <abhishek.chordia.ext@nokia.com>
2019-11-10 21:50:17 +0530

    initial commit

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 """Sample code to sum two values"""
-num1 = 15
+NUM1 = 15
 NUM2 = 12
-SUM = num1 + NUM2
-print("Sum of {0} and {1} is {2}" .format(num1, NUM2, SUM))
+SUM = NUM1 + NUM2
+print("Sum of {0} and {1} is {2}" .format(NUM1, NUM2, SUM))
